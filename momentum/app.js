c3cb095850c952b13034d65d6323748da0668db2
Pablaw <chulhwa7@gmail.com>
2021-12-04 17:36:39 +0900

    Second push

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,7 @@
-console.log('a');
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sta'];
+
+// Get Intem from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to array
+daysOfWeek.push('sun');
