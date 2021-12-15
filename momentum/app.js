ec4248356779bee9a86268dd2cf707c569fa8140
Pablaw <chulhwa7@gmail.com>
2021-12-15 17:58:55 +0900

    Dec 3rd Wed Leave Work

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,19 @@
-console.log('a');
+// #2.10 Recap II (12:52)
+const calculator = {
+    add: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b) {
+        console.log(a - b)
+    },
+    divide: function(a, b) {
+        console.log(a / b)
+    },
+    powerOf: function(a, b) {
+        console.log(a ** b);
+    }
+}
+calculator.add(4, 2);
+calculator.minus(4, 2);
+calculator.divide(4, 2);
+calculator.powerOf(4, 2);
