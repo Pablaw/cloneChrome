4c366d34d3016bcf81238fbba9e1aebb0af0db1f
Pablaw <chulhwa7@gmail.com>
2022-01-22 21:01:18 +0900

    Jan 3rd Sat

---
+++ assignment.js
@@ -0,0 +1,14 @@
+/* Submit Assignment 1
+https://codesandbox.io/s/ecstatic-wood-j3h5o?file=/src/index.js */
+
+/* Submit Assignment 2
+https://codesandbox.io/s/beautiful-keller-zk292?file=/src/index.js */
+
+/* Submit Assignment 3
+https://codesandbox.io/s/dreamy-glitter-pu3xd?file=/src/index.js */
+
+/* Submit Assignment 4
+https://codesandbox.io/s/a08blueprint-forked-2ezmv?file=/src/index.js */
+
+/* Submit Assignment 5
+https://codesandbox.io/s/a09blueprint-forked-t99dp?file=/src/index.js */
--- test.js
+++ test.js
@@ -1,69 +1,33 @@
-const clockTitle = document.querySelector(".js-clock");
-const christmas = 365 - 6;
+var provider = new firebase.auth.GoogleAuthProvider();
 
-// monthDayArr = {Jan: 31, Feb: 28, Mac: 31, Apr: 30, May: 31, Jun: 30, Jul: 31, Aug: 31, Sep: 30, Oct: 31, Nov: 30, Dec: 31};
+provider.addScope('https://accounts.google.com/o/oauth2/v2/auth');
 
-const month = new Date().getMonth();
-let daySum = 0;
-switch (month) {
-  case 1:
-    daySum = 31;
-      break;
-  case 2:
-    daySum = 59;
-      break;
-  case 3:
-    daySum = 90;
-      break;
-  case 4:
-    daySum = 120;
-      break;
-  case 5:
-    daySum = 151;
-      break;
-  case 6:
-    daySum = 181;
-      break;
-  case 7:
-    daySum = 212;
-      break;
-  case 8:
-    daySum = 243;
-      break;
-  case 9:
-    daySum = 273;
-      break;
-  case 10:
-    daySum = 304;
-      break;  
-  case 11:
-    daySum = 334;
-      break;  
-}
+provider.setCustomParameters({
+  'login_hint': 'user@example.com'
+});
 
-function displayClock () {
-  let restDays = christmas - (daySum + new Date().getDate());
-  let restHours = 23 - new Date().getHours();
-  let restMinutes = 59 - new Date().getMinutes();
-  let restSeconds = 59 - new Date().getSeconds();
+firebase.auth().signInWithRedirect(provider);
 
-  clockTitle.innerHTML = `${restDays}d ${restHours}h ${restMinutes}m ${restSeconds}s`;
-}
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
 
-displayClock();
-setInterval(displayClock, 1000);
-
-/* Submit Assignment 1
-https://codesandbox.io/s/ecstatic-wood-j3h5o?file=/src/index.js */
-
-/* Submit Assignment 2
-https://codesandbox.io/s/beautiful-keller-zk292?file=/src/index.js */
-
-/* Submit Assignment 3
-https://codesandbox.io/s/dreamy-glitter-pu3xd?file=/src/index.js */
-
-/* Submit Assignment 4
-https://codesandbox.io/s/a08blueprint-forked-2ezmv?file=/src/index.js */
-
-/* Submit Assignment 5
-https://codesandbox.io/s/a09blueprint-forked-t99dp?file=/src/index.js */
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
