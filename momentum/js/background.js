1a4e1cd7f364013a10130cae0b3c65ad7c29ab67
Pablaw <chulhwa7@gmail.com>
2022-01-23 20:39:59 +0900

    Jan 4th Sun Night

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,4 +1,4 @@
-const images = [
+/* const images = [
     "1.jpg",
     "2.jpg",
     "3.jpg",
@@ -14,4 +14,38 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage); // appendChild() => end < > prependChild()
+document.body.appendChild(bgImage); // appendChild() => end < > prependChild() */
+
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+  
+  function changeColors() {
+    const colorOne = colors[Math.floor(Math.random() * colors.length)];
+    const colorTwo = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.background = `linear-Gradient(to right, ${colorOne}, ${colorTwo})`;
+    document.body.classList.toggle("change");
+    
+  }
+  
+  changeColors();
+setInterval(changeColors, 5000);
+  
--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -9,7 +9,7 @@ function onGeoSuccess(position) {
         .then(data => {
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            weather.innerText = `${data.weather[0].main} / Temp: ${data.main.temp}`;
             city.innerText = data.name;
         });
 }
